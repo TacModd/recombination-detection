c8c7e57b2b91a0a42b792f2dd7f81b1d7fc5a200
Macgregor <maccyjd@gmail.com>
2018-04-21 01:09:09 +1000

    Create mytest1.R

---
+++ R/mytest1.R
@@ -0,0 +1,27 @@
+# takes returned value of partitions function plus desired threshold
+# iteratively calculates probability of k + 1 or more sampled in n
+# for all possible k with first and last k indices forming bounds of n
+spt1 = function(ptns, sig){
+  results = list()
+  for (i in 1:length(ptns$pattern.IDs)){
+    indices = which(ptns$pattern.indices == i)
+    p = length(indices)/length(ptns$pattern.indices)
+    if (ptns$pattern.counts[i] > 1){
+      tempcount = 0
+      tempvector = list()
+      for (j in 1:(length(indices) - 1)){
+        for (k in 1:(length(indices) - j)){
+          n = indices[j+k] - indices[j] + 1
+          r = 1 - pbinom(k, n, p)
+          if (r <= sig){
+            tempcount = tempcount + 1
+            tempvector[[tempcount]] = c(indices[j], indices[j + k], k + 1, n, r)
+          }
+          
+        }
+      }
+      results[[i]] = tempvector
+    }
+  }
+  results
+}
