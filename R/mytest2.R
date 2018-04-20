fa5cad5ece6cd3a23bd4c460cd0b67fcf347faa1
Macgregor <maccyjd@gmail.com>
2018-04-21 01:10:19 +1000

    Create mytest2.R

---
+++ R/mytest2.R
@@ -0,0 +1,22 @@
+incomplete
+# window based on set n
+# for i in range pattern.IDs
+spt2 = function(ptns, i){
+  indices = which(ptns$pattern.indices == i)
+  p = length(indices)/length(ptns$pattern.indices)
+  n = 2
+  
+  if (ptns$pattern.counts[i] > 1){
+    while (1 - (1-p)**n - n*(p)*(1-p)**(n-1) < 0.05){
+      n = n + 1
+    }
+    
+    for (j in range(1:length(indices) - 1)){
+      q = length(which(ptns$pattern.indices[j:j+n] == i)) - 1
+      if (length(q) > 1){
+        r = 1 - pbinom(q, n, p)
+      }
+    }
+    r
+  }
+}
