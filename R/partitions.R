fede828ef9008e70f956f5c40d6e587d9ffd2f08
Macgregor <maccyjd@gmail.com>
2018-05-06 16:24:00 +1000

    Update partitions.R

--- R/partitions.R
+++ R/partitions.R
@@ -1,8 +1,8 @@
 # this function returns a partition object, which includes the number of unique partition
 # patterns, their ids, their indices, and their counts
 partitions = function(patterns){
-  #constant.sites = unique(grep('1{66}', GC2pttns, value = TRUE))
-  #sites = GC2pttns[GC2pttns != constant.sites]
+  #constant.sites = unique(grep(as.character(paste('1{', nchar(patterns[1]), '}', sep='')), patterns, value = TRUE))
+  #sites = [patterns[patterns != constant.sites]
   # this way is faster
   sites = unique(patterns[patterns != as.character(paste(rep(1, nchar(patterns[1])), collapse=''))])
   list(patterns = sites, pattern.IDs = match(sites, sites), pattern.indices = match(patterns, sites),
