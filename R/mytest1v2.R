8719f8891e8acc96f5b3d9f9e4363f718f55b4e4
Macgregor <maccyjd@gmail.com>
2018-05-20 19:30:58 +1000

    Update mytest1v2.R

--- R/mytest1v2.R
+++ R/mytest1v2.R
@@ -1,6 +1,6 @@
 # alternative implementation of test 1
 
-spt1 = function(ptns, sig){
+spt1v2 = function(ptns, sig){
   results = list()
   for (i in 1:length(ptns$pattern.IDs)){
     if (ptns$pattern.counts[i] > 2){
@@ -8,15 +8,15 @@ spt1 = function(ptns, sig){
       p = length(indices)/length(ptns$pattern.indices)
       tempvector = list()
       j = 1
-      while (j < (length(ptns)-1)){
-        n1 = ptns[j+2]-ptns[j]+1
+      while (j < (length(indices)-1)){
+        n1 = indices[j+2]-indices[j]+1
         r1 = 1 - pbinom(2, n1, p)
         if (r1 <= sig){
           k = j+2
-          while (k < length(ptns)){
-            n1 = ptns[k]-ptns[j]+1
+          while (k < length(indices)){
+            n1 = indices[k]-indices[j]+1
             r1 = 1 - pbinom((k-j), n1, p)
-            n2 = ptns[k+1]-ptns[j]+1
+            n2 = indices[k+1]-indices[j]+1
             r2 = 1 - pbinom((k-j+1), n2, p)
             
             if (r2 <= r1){
@@ -25,8 +25,8 @@ spt1 = function(ptns, sig){
               break
             }
           }
-          n = ptns[k]-ptns[j]+1
-          lp = p^(k-j+1) * (ptns[k]-ptns[j]+1)
+          n = indices[k]-indices[j]+1
+          lp = p^(k-j+1) * (indices[k]-indices[j]+1)
           tempvector[[j]] = c(j, k, k-j+1, n, lp)
           j = j + k
         } else {
