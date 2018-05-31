55dcfd7f9a7f3922cc1aa2408a7b909e206faa12
Macgregor <maccyjd@gmail.com>
2018-05-31 18:42:23 +1000

    Update stephtest1.R

--- R/stephtest1.R
+++ R/stephtest1.R
@@ -1,4 +1,4 @@
-# first of stephens' tests
+# first of stephens' tests - corresponds to equation 5 in paper
 lengthdtest = function(n, d, s){
   numerator = n*choose(d, s - 1) - (s - 1)*choose(d + 1, s)
   denominator = choose(n, s)
