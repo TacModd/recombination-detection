c233d19db30d950bec74e6c6b8777048279b5313
Macgregor <maccyjd@gmail.com>
2018-06-25 10:46:16 +1000

    Update and rename mytest3.R to rec.testcluster.R
    - return fit so user can cut the tree according to height to obtain 'significant clusters'

--- R/mytest3.R
+++
@@ -1,9 +0,0 @@
-# 'test' based on clustering and height cutoff
-
-spt3 = function(ptns, i){
-  indices = which(ptns$pattern.indices == i)
-  fit = cmdscale(dist(indices), eig=FALSE, k=1)
-  plot(fit[, 1], rep(0, length(indices)), pch = 19)
-  fit2 = hclust(dist(indices), method = 'ward.D2')
-  plot(fit2)
-}
