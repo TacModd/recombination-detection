aeb9dc9f8e710765114e917342ef754e5ca9dcd0
Macgregor <maccyjd@gmail.com>
2018-07-08 18:38:22 +1000

    Update patterns.R

--- R/patterns.R
+++ R/patterns.R
@@ -1,12 +1,20 @@
-# patterns generates integers for columns of sites: the order of these integers 
-# reflects the pattern of site base variation across sequences
-patterns = function(sequences){
-  pttns = matrix(nrow = nrow(sequences), ncol = ncol(sequences))
+# takes a multiple aligned sequence object and generates integer 
+# sequences (patterns) representing columns of sites: these patterns 
+# reflect the pattern of site base variation across DNA sequences. 
+# returns a pattern object.
+get.patterns = function(sequences){
+  # initialise matrix
+  patterns = matrix(nrow = nrow(sequences), ncol = ncol(sequences))
+  # for each column in the aligned sequences:
   for (i in 1:ncol(sequences)){
+    # get the unique values
     k = unique(sequences[, i])
+    # generate the patterns (by matching against the vector of
+    # unique values)
     for (j in 1:nrow(sequences)){
-      pttns[j, i] = match(sequences[j, i], k)
+      patterns[j, i] = match(sequences[j, i], k)
     }
   }
-  pttns
+  # return the patterns object
+  patterns
 }
