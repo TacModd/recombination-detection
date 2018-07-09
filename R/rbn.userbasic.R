a51ff665d90c1c2632e7c3f2659c8b105ae07919
Macgregor <maccyjd@gmail.com>
2018-07-09 14:50:57 +1000

    Update rbn.userbasic.R
    - have to stop prematurely: saving and committing current work

--- R/rbn.userbasic.R
+++ R/rbn.userbasic.R
@@ -1,23 +1,38 @@
 # implements (basic) user set window method
 
 rbn.userbasic = function(partitions, sig, n){
+  # initialise results object
   results = list()
+  # initialise a count value to keep track of recombined partitions
   outertempcount = 0
+  # for each unique partition ID:
   for (i in 1:length(partitions$pattern.IDs)){
+    # if there are at least 3 partitions belonging to said ID:
     if (partitions$pattern.counts[i] > 2){
+      # get the partition indices
       indices = which(partitions$pattern.indices == i)
+      # get the partition probability
       p = length(indices)/length(partitions$pattern.indices)
+      # initialise a count value to keep track of detected rbn events
       innertempcount = 0
+      # initalise a vector to store event details
       tempvector = list()
+      # initialise a marker equal to the 1st partition index
       j = indices[1]
+      # while we haven't iterated over all members of the current ptn:
       while (j < indices[length(indices)]){
+        #
         tempindices = which(partitions$pattern.indices[j:(j+n-1)] == i)
+        #
         tempindices = tempindices + j - 1
+        #
         q = length(tempindices) - 1
-        r1 = 1 - pbinom(q, n, p)
-        if (r1 <= sig & q > 0){
+        #
+        r1 = 1 - pbinom(q, n, p) # fix
+        #
+        if (r1 <= sig & q > 0){ # fix?
           n = tempindices[length(tempindices)] - j
-          sigval = 1 - pbinom(q, n, p)
+          sigval = 1 - pbinom(q, n, p) # doesn't need fixing
           tempvector[[j]] = c(i, j, tempindices[length(tempindices)], q+1, n, sigval)
           innertempcount = innertempcount + 1
           j = tempindices[q + 1] + 1
