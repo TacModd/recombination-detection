a68265e4d5bfc445ca1ec8b4d1678750ecf39c62
Macgregor <maccyjd@gmail.com>
2018-07-09 12:48:10 +1000

    Update plot.ptn.R

--- R/plot.ptn.R
+++ R/plot.ptn.R
@@ -4,8 +4,6 @@
 plot.ptn = function(partitions, i){
   # get indices of sites for ith partition
   indices = which(partitions$pattern.indices == i)
-  #fit = cmdscale(dist(indices), eig=FALSE, k=1)
-  #plot(fit[, 1], rep(0, length(indices)), pch=19)
   # plot sites
   plot(indices, rep(0, length(indices)), pch=19, xlim = c(0, length(partitions$pattern.indices)), 
        yaxt='n', xlab='Partition positions in genome', ylab='')
