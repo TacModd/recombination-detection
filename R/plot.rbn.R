4890accf2448fc607a7ad61235b9a7f71978e173
Macgregor <maccyjd@gmail.com>
2018-07-09 12:59:41 +1000

    Update plot.rbn.R

--- R/plot.rbn.R
+++ R/plot.rbn.R
@@ -1,18 +1,20 @@
 # takes a partition object, a result object and an integer 
-# for the jth result. determines the ith partition from j and
+# for the kth result. determines the ith partition from k and
 # plots the distribution of sites from the ith partition AND 
-# the recombined sites from the jth result across the length 
+# the recombined sites from the kth result across the length 
 # of the genome. recombined regions are highlighted in pink
 
-plot.rbn = function(partitions, results, j){
-  i = res[[j]][1, 1]
+plot.rbn = function(partitions, results, k){
+  # get ith partition from k
+  i = res[[k]][1, 1]
+  # get indices of sites for ith partition
   indices = which(partitions$pattern.indices == i)
-  #fit = cmdscale(dist(indices), eig=FALSE, k=1)
-  #plot(fit[, 1], rep(0, length(indices)), pch=19)
+  # plot the partition sites
   plot(indices, rep(0, length(indices)), pch=19, xlim = c(1, length(partitions$pattern.indices)), 
       yaxt='n', xlab='Partition positions in genome', ylab='')
-  for (k in 1:nrow(results[[j]])){
-    points(results[[j]][k, 2:3], c(0, 0), col='red', pch = 19)
-    lines(results[[j]][k, 2:3], c(0, 0), lwd = c(100, 1), lend = 1, col='pink')
+  # overlap recombined sites according to kth result
+  for (j in 1:nrow(results[[k]])){
+    points(results[[k]][j, 2:3], c(0, 0), col='red', pch = 19)
+    lines(results[[k]][j, 2:3], c(0, 0), lwd = c(100, 1), lend = 1, col='pink')
   }
 }
