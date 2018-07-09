c779af0f9a169f05f89e71e3018bc7868155d12b
Macgregor <maccyjd@gmail.com>
2018-07-09 17:39:13 +1000

    Update rbn.userbasic.R
    - finished commenting; several lines that need fixing identified

--- R/rbn.userbasic.R
+++ R/rbn.userbasic.R
@@ -15,52 +15,74 @@ rbn.userbasic = function(partitions, sig, n){
       p = length(indices)/length(partitions$pattern.indices)
       # initialise a count value to keep track of detected rbn events
       innertempcount = 0
-      # initalise a vector to store event details
+      # initalise a vector to temporarily store event details
       tempvector = list()
-      # initialise a marker equal to the 1st partition index
-      j = indices[1]
-      # while we haven't iterated over all members of the current ptn:
+      # initialise a left bound marker equal to the 1st partition index
+      j = indices[1] # this introduces a bias on the first test - change?
+      # while we haven't iterated past the last ptn index:
       while (j < indices[length(indices)]){
-        #
+        # get indices between j and j + user window size
         tempindices = which(partitions$pattern.indices[j:(j+n-1)] == i)
-        #
+        # correct index values (above values from 1)
         tempindices = tempindices + j - 1
-        #
-        q = length(tempindices) - 1
-        #
-        r1 = 1 - pbinom(q, n, p) # fix
-        #
-        if (r1 <= sig & q > 0){ # fix?
-          n = tempindices[length(tempindices)] - j
-          sigval = 1 - pbinom(q, n, p) # doesn't need fixing
-          tempvector[[j]] = c(i, j, tempindices[length(tempindices)], q+1, n, sigval)
+        # get the number of 'events' (partitions) within window
+        k = length(tempindices)
+        # calculate the probability of k or more events
+        r1 = 1 - pbinom(k-1, n, p) # fix
+        # if probability below significance threshold:
+        if (r1 <= sig & k > 0){ # fix?
+          # no point in below line, should probably be removed
+          #n = tempindices[length(tempindices)] - j
+          # recalculate probability
+          sigval = 1 - pbinom(k-1, n, p) # doesn't need fixing
+          # store event details (needs fixing)
+          tempvector[[j]] = c(i, j, tempindices[length(tempindices)], k, n, sigval)
+          # update rbn event count
           innertempcount = innertempcount + 1
-          j = tempindices[q + 1] + 1
+          # update left bound marker
+          j = tempindices[q + 1] + 1 # not wrong but should be changed (simplified)
+          
+        # if not below significance threshold (should also be simplified probably)
         } else {
+          # if there were actually partitions in window to test
           if (length(tempindices) > 0){
+            # if marker equals the first ptn index
             if (j == tempindices[1]){
+              # update it to the next
               j = j + 1
+            # otherwise update it to the first
             } else {
               j = tempindices[1]
             }
+          # otherwise add window size to marker
           } else {
-            j = j + n + 1
+            j = j + n
           }
         }
       }
+      # initialise matrix to store results for ith partition
       tempmatrix = matrix(0, nrow=innertempcount, ncol=6)
+      # reset rbn event count (line should go below check)
       innertempcount = 1
+      # if at least 1 rbn event was found:
       if (length(tempvector) > 0){
+        # for each event:
         for (j in 1:length(tempvector)){
+          # if the event is not null (is this check necessary??):
           if (!is.null(tempvector[[j]])){
+            # add event details to matrix
             tempmatrix[innertempcount, ] = tempvector[[j]]
+            # update rbn event count
             innertempcount = innertempcount + 1
           }
         }
+        # update recombined ptns count
         outertempcount = outertempcount + 1
+        # add matrix for ith ptn to result list
         results[[outertempcount]] = tempmatrix
       }
     }
   }
+  # return result object
   results
 }
