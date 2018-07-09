ecc1f32d5e93bd2c8b1af4146fe0bfa15a36213b
Macgregor <maccyjd@gmail.com>
2018-07-09 13:05:02 +1000

    Update plot.rbns.R

--- R/plot.rbns.R
+++ R/plot.rbns.R
@@ -3,15 +3,17 @@
 # according to result object
 
 plot.rbns = function(partitions, results){
+  # plot an empty frame the length of the genome
   plot(1, type='n', ylim = c(-1, 1), xlim = c(1, length(partitions$pattern.indices)), 
       yaxt='n', xlab='Recombination positions in genome', ylab='')
-  for (i in 1:length(results)){
-    #j = res[[i]][1, 1]
-    #indices = which(ptns$pattern.indices == j)
-    #points(indices, rep(0, length(indices)), pch=19)
-    for (k in 1:nrow(results[[i]])){
-      points(results[[i]][k, 2:3], c(0, 0), col='red', pch = 19)
-      lines(results[[i]][k, 2:3], c(0, 0), lwd = c(100, 1), lend = 1, col='pink')
+  # then for each recombined partition according to result object
+  for (k in 1:length(results)){
+    #i = res[[k]][1, 1]
+    #indices = which(ptns$pattern.indices == i)
+    # overlap each recombined site according to kth result
+    for (j in 1:nrow(results[[k]])){
+      points(results[[k]][j, 2:3], c(0, 0), col='red', pch = 19)
+      lines(results[[k]][j, 2:3], c(0, 0), lwd = c(100, 1), lend = 1, col='pink')
     }
   }
 }
