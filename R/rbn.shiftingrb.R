9741d689771a40e7ae605522d1370969c08281f1
Macgregor <maccyjd@gmail.com>
2018-07-11 14:25:10 +1000

    Update rbn.shiftingrb.R

--- R/rbn.shiftingrb.R
+++ R/rbn.shiftingrb.R
@@ -1,54 +1,92 @@
 # expands right bound while test <= prior test; otherwise right bound forms left bound of new test (linear complexity)
 
 rbn.shiftingrb = function(partitions, sig){
+  # initialise result object
   results = list()
+  # initialise a count value to keep track of recombined partitions
   outertempcount = 0
+  # for each unique partition ID:
   for (i in 1:length(partitions$pattern.IDs)){
+    # if there are at least 3 partitions belonging to said ID:
     if (partitions$pattern.counts[i] > 2){
+      # get the partition indices
       indices = which(partitions$pattern.indices == i)
+      # get the partition probability
       p = length(indices)/length(partitions$pattern.indices)
+      # initialise a count value to keep track of detected rbn events
       innertempcount = 0
+      # initalise a vector to temporarily store event details
       tempvector = list()
+      # initialise a left bound marker
       j = 1
+      # while we haven't iterated past the 2nd last ptn:
       while (j < (length(indices)-1)){
+        # calculate window size to include 3 partitions
         n1 = indices[j+2]-indices[j]+1
+        # calculate probability of 3 or more events
         r1 = pbinom(2, n1, p, log=TRUE) # r1 = 1 - pbinom(2, n1, p) # 
+        # probability below significant threshold:
         if (r1 > log(1 - sig)){ # (r1 <= sig)
+          # set a right bound marker
           k = j+2
+          # while we haven't iterated past the last ptn:
           while (k < length(indices)){
+            # calculate the probability of k-j+1 events
             n1 = indices[k]-indices[j]+1
             r1 = pbinom((k-j), n1, p, log=TRUE) # r1 = 1 - pbinom((k-j), n1, p) # 
+            # calculate the probability of k-j+2 events
             n2 = indices[k+1]-indices[j]+1
             r2 = pbinom((k-j+1), n2, p, log=TRUE) # r2 = 1 - pbinom((k-j+1), n2, p) # 
-            
+            # note without talking about how equations are arranged saying increases log
+            # probability may cause confusion - consider changing
+            # if increasing the window reduces probability (increases log probability)
             if (r2 >= r1){ # (r2 <= r1)
+              # keep expanding window (via right bound)
               k = k + 1
+            # otherwise stop and start a new window
             } else {
               break
             }
           }
+          # get the number of events
           n = indices[k]-indices[j]+1
-          sigval = 1 - pbinom((k-j), n, p)
-          tempvector[[j]] = c(i, indices[j], indices[k], k-j+1, n, sigval)
+          # get the log probability
+          logsigval = pbinom((k-j), n, p, log=TRUE)
+          # # store event details
+          tempvector[[j]] = c(i, indices[j], indices[k], k-j+1, n, 1 - exp(logsigval))
+          # update rbn event count
           innertempcount = innertempcount + 1
+          # update left bound marker to partition following right bound marker
           j = j + k
+        # if starting window not significant:
         } else {
+          # update left bound to next partition
           j = j + 1
         }
       }
+      # initialise matrix to store results for ith partition
       tempmatrix = matrix(0, nrow=innertempcount, ncol=6)
-      innertempcount = 1
+      # if at least 1 rbn event was found:
       if (length(tempvector) > 0){
+        # reset rbn event count
+        innertempcount = 1
+        # for each event:
         for (j in 1:length(tempvector)){
+          # if the event is not null (is this check necessary??):
           if (!is.null(tempvector[[j]])){
+            # add event details to matrix
             tempmatrix[innertempcount, ] = tempvector[[j]]
+            # update rbn event count
             innertempcount = innertempcount + 1
           }
         }
+        # update recombined ptns count
         outertempcount = outertempcount + 1
+        # add matrix for ith ptn to result list
         results[[outertempcount]] = tempmatrix
       }
     }
   }
+  # return result object
   results
 }
