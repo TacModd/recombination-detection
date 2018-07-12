3b7d27eab900cd04571a521d8226c1330546e46b
Macgregor <maccyjd@gmail.com>
2018-07-12 18:38:55 +1000

    Update mask.R

--- R/mask.R
+++ R/mask.R
@@ -1,11 +1,10 @@
-# takes a multiple alignment object, a partition object and a result object.
+# takes a multiple alignment (DNAbin) object, a partition object and a result object.
 # masks sites in the multiple alignment object that are recombined 
 # according to result object
-# need to test that it's working on toy data
+
 mask = function(sequences, partitions, results){
-  # this line might be introducing a bug -- need to confirm
-  ## line confirmed harmless for matrices and necessary for (DNAbin) lists ##
-  # sequences = as.matrix(sequences)
+  # convert sequences to matrix in case sequences in list format
+  sequences = as.matrix(sequences)
   # for each recombined partition:
   for (i in 1:length(results)){
     # find the sites that accord with that partition
@@ -16,9 +15,10 @@ mask = function(sequences, partitions, results){
       tempm = c(which(indices == results[[i]][j, 2]), which(indices == results[[i]][j, 3]))
       # and mask each partition site that falls within those bounds (inclusive)
       sequences[, indices[tempm[1]:tempm[length(tempm)]]] = as.DNAbin('n')
-      #sequences[, indices[tempm[1]]:indices[tempm[length(tempm)]]] = as.DNAbin('n')
     }
   }
+  # could convert back to list but not necessary (better to leave up to user)
+  # sequences = as.list(sequences)
   # return the masked alignment object
   sequences
 }
